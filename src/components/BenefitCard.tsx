96b303698f06c4ba5ec17d33fc65b70d8bb5b8b5
Renth <135339655+Arch-Ren@users.noreply.github.com>
2026-03-27 10:18:02 +0700

    style(benefit)
    typo

--- src/components/BenefitCard.tsx
+++ src/components/BenefitCard.tsx
@@ -9,7 +9,7 @@ interface benefitCardProps {
 
 export default function BenefitCard({gambar, benefit} : benefitCardProps) {
     return (
-        <div className="bg-white rounded-3xl justify-items-center content-center w-[417px] h-[402px] shadow-2xl/40">
+        <div className="bg-white rounded-3xl justify-items-center content-center w-[417px] h-[402px] shadow-2xl">
             <img src={gambar} alt={benefit} className="scale-80"></img>
             <h3 className="w-[357px] h-[110px] text-[34px] text-center">{benefit}</h3>
         </div>
